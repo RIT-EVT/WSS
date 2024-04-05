6b50ab609ed7132aa4f0930e9531425ad47a7078
Oleg <lazarioleg97@gmail.com>
2024-04-04 20:20:09 -0400

    added can support

--- src/WSS.cpp
+++ src/WSS.cpp
@@ -1,3 +1,30 @@
-//#include <BOARD_NAME.hpp>
+#include "WSS.hpp"
 
-namespace BOARD_NAME {}
+namespace WSS {
+
+WSS::WSS(hallSensor::hallSensor& hallSensor1, hallSensor::hallSensor& hallSensor2) {
+    this->hallSensors[0] = &hallSensor1;
+    this->hallSensors[1] = &hallSensor2;
+}
+
+CO_OBJ_T* WSS::getObjectDictionary() {
+    return objectDictionary;
+}
+
+void WSS::process() {
+    for (uint8_t i = 0; i < NUM_HALLSENSORS; i++) {
+        timeDifference[i] = hallSensors[i]->update();
+        wheelSpeeds[i] = hallSensors[i]->getSpeed(timeDifference[i]);
+    }
+}
+
+uint8_t WSS::getNodeID() {
+    return NODE_ID;
+}
+
+uint8_t WSS::getNumElements() {
+    return OBJECT_DICTIONARY_SIZE;
+}
+
+} // namespace WSS
+
--- targets/REV3-WSS/main.cpp
+++ targets/REV3-WSS/main.cpp
@@ -7,8 +7,38 @@
 #include <EVT/io/UART.hpp>
 #include <EVT/io/pin.hpp>
 #include <EVT/manager.hpp>
+#include <WSS.hpp>
+#include <EVT/dev/MCUTimer.hpp>
+#include <EVT/io/CANOpenMacros.hpp>
+#include <EVT/io/CANopen.hpp>
+#include <EVT/io/UART.hpp>
+#include <EVT/io/pin.hpp>
+#include <EVT/manager.hpp>
+#include <EVT/utils/log.hpp>
+#include <EVT/utils/time.hpp>
 
 namespace IO = EVT::core::IO;
+namespace log = EVT::core::log;
+namespace time = EVT::core::time;
+namespace DEV = EVT::core::DEV;
+
+// create a can interrupt handler
+void canInterrupt(IO::CANMessage& message, void* priv) {
+    EVT::core::types::FixedQueue<CANOPEN_QUEUE_SIZE, IO::CANMessage>* queue =
+        (EVT::core::types::FixedQueue<CANOPEN_QUEUE_SIZE, IO::CANMessage>*) priv;
+
+    // Log raw received data
+    log::LOGGER.log(log::Logger::LogLevel::DEBUG, "Got RAW message from %X of length %d with data: ", message.getId(), message.getDataLength());
+
+    uint8_t* data = message.getPayload();
+    for (int i = 0; i < message.getDataLength(); i++) {
+        log::LOGGER.log(log::Logger::LogLevel::DEBUG, "%X ", *data);
+        data++;
+    }
+
+    if (queue != nullptr)
+        queue->append(message);
+}
 
 int main() {
     // Initialize system
@@ -17,13 +47,76 @@ int main() {
     // Setup UART
     IO::UART& uart = IO::getUART<IO::Pin::UART_TX, IO::Pin::UART_RX>(9600);
 
-    // String to store user input
-    char buf[100];
+    // Setup GPIO
+    IO::GPIO& interruptGPIO = IO::getGPIO<EVT::core::IO::Pin::PA_1>(
+        IO::GPIO::Direction::INPUT);
+    IO::GPIO& interruptGPIO2 = IO::getGPIO<EVT::core::IO::Pin::PA_0>(
+        IO::GPIO::Direction::INPUT);
+
+    uart.printf("GPIO Initialized\r\n");
+
+    // Setup Hall Sensor
+    // Wheel radius in inches
+    constexpr uint32_t WHEEL_RADIUS = 15;
+    constexpr uint32_t BACK_WHEEL_RADIUS = 15;
+
+    hallSensor::hallSensor hallSensor1(interruptGPIO, WHEEL_RADIUS);
+    hallSensor::hallSensor hallSensor2(interruptGPIO2, BACK_WHEEL_RADIUS);
+
+    // Setup WSS
+    WSS::WSS wss(hallSensor1, hallSensor2);
+
+    DEV::Timer& timer = DEV::getTimer<DEV::MCUTimer::Timer2>(100);
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Setup CAN configuration, this handles making drivers, applying settings.
+    // And generally creating the CANopen stack node which is the interface
+    // between the application (the code we write) and the physical CAN network
+    ///////////////////////////////////////////////////////////////////////////
+
+    // Will store CANopen messages that will be populated by the EVT-core CAN
+    // interrupt
+    EVT::core::types::FixedQueue<CANOPEN_QUEUE_SIZE, IO::CANMessage> canOpenQueue;
+
+    // Initialize CAN, add an IRQ which will add messages to the queue above
+    IO::CAN& can = IO::getCAN<IO::Pin::PA_12, IO::Pin::PA_11>();
+    can.addIRQHandler(canInterrupt, reinterpret_cast<void*>(&canOpenQueue));
+
+    // Attempt to join the CAN network
+    IO::CAN::CANStatus result = can.connect();
+
+    // Check to see if the device is connected to the CAN network
+    if (result != IO::CAN::CANStatus::OK) {
+        log::LOGGER.log(log::Logger::LogLevel::ERROR, "Failed to connect to CAN network\r\n");
+        return 1;
+    } else {
+        log::LOGGER.log(log::Logger::LogLevel::INFO, "Connected to CAN network\r\n");
+    }
+
+    // Reserved memory for CANopen stack usage
+    uint8_t sdoBuffer[CO_SSDO_N * CO_SDO_BUF_BYTE];
+    CO_TMR_MEM appTmrMem[16];
+
+    // Reserve driver variables
+    CO_IF_DRV canStackDriver;
+
+    CO_IF_CAN_DRV canDriver;
+    CO_IF_TIMER_DRV timerDriver;
+    CO_IF_NVM_DRV nvmDriver;
+
+    CO_NODE canNode;
+
+    // Initialize all the CANOpen drivers.
+    IO::initializeCANopenDriver(&canOpenQueue, &can, &timer, &canStackDriver, &nvmDriver, &timerDriver, &canDriver);
+
+    // Initialize the CANOpen node we are using.
+    IO::initializeCANopenNode(&canNode, &wss, &canStackDriver, sdoBuffer, appTmrMem);
+
+    // Set the node to operational mode
+    CONmtSetMode(&canNode.Nmt, CO_OPERATIONAL);
 
     while (1) {
-        // Read user input
-        uart.printf("Enter message: ");
-        uart.gets(buf, 100);
-        uart.printf("\n\recho: %s\n\r", buf);
+        wss.process();
+        IO::processCANopenNode(&canNode);
     }
 }
